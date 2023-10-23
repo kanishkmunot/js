8c5894807c7f00b1d8a5cac04f6cb3a55b66e9f4
kanishkmunot <128204622+kanishkmunot@users.noreply.github.com>
2023-10-23 21:01:04 +0000

    variable and dataTypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "abc@gmail.com"
+var accountPassword = "54321"
+accountCity = "Ahmedabad"
+
+// accountId = 1 // not allowed
+
+accountEmail = "bac@gmail.com"
+accountPassword = "5987"
+accountCity = "Gandhinagar"
+console.log(accountId); 
+console.table([accountId,accountEmail,accountPassword, accountCity])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use Strict"; // treat all js code as newer version
+// alert(6+5) // we are using nodejs not browser
+console.log("kenny");
+console.log(123456);
+
+// boolean 
+// number
+// null
+// string
+// bigInt
+// undefined
+// symbol
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
