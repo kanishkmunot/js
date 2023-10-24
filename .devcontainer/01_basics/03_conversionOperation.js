d6fe5d960adde2dc4a7d08b44aabfe7c5bba809f
kanishkmunot <128204622+kanishkmunot@users.noreply.github.com>
2023-10-24 04:22:31 +0000

    3 and 4 added

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -5,3 +5,22 @@ console.log(typeof score);
 let valueinNumber = Number(score)
 console.log(typeof valueinNumber);
 console.log(valueinNumber);
+
+// *******Operations********
+let value = 4
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "ninja"
+let str3 = str1+str2
+console.log(str3);
+console.log('56'+ 1 + 4 );
+console.log(5 + 4 + '56' );
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+
---
+++ .devcontainer/01_basics/04_comparisons.js
@@ -0,0 +1,14 @@
+console.log("7" > 3);
+console.log("7a" > 3);
+console.log(null>0);
+console.log(null>=0);
+console.log(undefined>=0);
+console.log(5676757>undefined);
+// comparison and equality check are two different things
+// The strict equality (===) operator checks whether its two operands are equal, returning a Boolean result. Unlike the equality operator, the strict equality operator always considers operands of different types to be different.
+
+console.log("2"===2);
+
+
+
+
