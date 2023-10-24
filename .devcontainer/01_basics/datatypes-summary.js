781407144195ba6f5e40ec93f949182e3bf57ef5
kanishkmunot <128204622+kanishkmunot@users.noreply.github.com>
2023-10-24 05:11:52 +0000

    summary of datatypes

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+// There are 7 types of Primitive datatypes
+
+// String, Number, Boolean , null (empty), symbol, BigInt, undefined
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const score = 235.42
+console.log(score);
+let userEmail;
+const id = Symbol('123')
+console.log(id);
+
+const heros = ['ca','im','thor','hawk-i']
