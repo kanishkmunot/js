f418f3d06bac02c20b9d35214a527085f83f3eef
kanishkmunot <128204622+kanishkmunot@users.noreply.github.com>
2023-11-09 11:53:23 +0000

    yo

--- .devcontainer/03_basics/05_javascriptececontext.js
+++ .devcontainer/03_basics/05_javascriptececontext.js
@@ -1,13 +1,15 @@
-1. Global execution context (inside "this")
-2. Function/Functional execution context
-3. Eval Execution context
+// 1. Global execution context (inside "this")
+// 2. Function/Functional execution context
+// 3. Eval Execution context
 
-=> Memory creation phase then Execution phase 
+// => Memory creation phase then Execution phase 
 
-Step 1. (as mentioned in the first line)
-Step 2. Memory phase => All the variables are put together and kept AND asssigned as UNDEFINED
-        Declared function gets a definition
+// Step 1. (as mentioned in the first line)
+// Step 2. Memory phase => All the variables are put together and kept AND asssigned as UNDEFINED
+//         Declared function gets a definition
 
-New Executional Context (i.e. new environment variable + execution thread are created) is created when arguments are passed into the function call.
+// => New Executional Context (i.e. new environment variable + execution thread are created) is created when arguments are passed into the function call.
 
 
+// => Call Stack 
+ 
