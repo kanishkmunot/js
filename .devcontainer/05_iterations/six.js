5253fa65566df3bdeca8ce0dfaa396fa548a347e
kanishkmunot <128204622+kanishkmunot@users.noreply.github.com>
2023-11-10 11:00:54 +0000

    six

--- .devcontainer/05_iterations/six.js
+++ .devcontainer/05_iterations/six.js
@@ -39,9 +39,9 @@ const books = [
   ];
 
   let userBooks = books.filter( (bk) => bk.genre === 'History')
-  console.log(userBooks);
-
-//   userBooks = books.filter( (bk) => { 
-//     return bk.publish >= 1995 && bk.genre === "History"
-// })
 //   console.log(userBooks);
+
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+  console.log(userBooks);
