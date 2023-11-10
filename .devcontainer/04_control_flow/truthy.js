ea3ffc8948a63afea5f5d57daa5e2ec7a6f33b64
kanishkmunot <128204622+kanishkmunot@users.noreply.github.com>
2023-11-10 07:53:47 +0000

    truthy2

--- .devcontainer/04_control_flow/truthy.js
+++ .devcontainer/04_control_flow/truthy.js
@@ -1,10 +1,10 @@
-const userEmail = []
+// const userEmail = []
 
-if (userEmail) {
-    console.log("Got user email");
-} else{
-    console.log("Don't have user email");
-}
+// if (userEmail) {
+//     console.log("Got user email");
+// } else{
+//     console.log("Don't have user email");
+// }
 
 // falsy values
 
@@ -26,3 +26,16 @@ if (userEmail) {
 
 
 
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+val1 = null ?? 10 //It's like a fallback, when we want to assign a value
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");;
