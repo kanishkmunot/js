e2ee661b211135f1be79eec9e631f636773b4ecd
kanishkmunot <128204622+kanishkmunot@users.noreply.github.com>
2023-11-10 07:27:26 +0000

    yorkshire

--- .devcontainer/04_control_flow/one.js
+++ .devcontainer/04_control_flow/one.js
@@ -8,3 +8,36 @@ if (false) {
 }
 
 // ==, <, >, >=, <=, !=
+
+const temperature = 46
+if(temperature<50){
+    console.log("Less than 50");
+}else
+{console.log("Temperature is greater than 50");}
+
+
+const score = 300
+
+if (score>200) {
+    const power="fly"
+    console.log(`User power: ${power}`);
+}
+
+
+const balance = 400
+if (balance>500) console.log("test"); //IMPLICIT SCOPE (ONLY FOR A SINGLE LINE)
+
+if (balance<500) {
+    console.log("less than");
+} else if (balance < 750) {
+    console.log("Less than 750");
+} else {
+    console.log("Less than 1200");
+}
+
+const userLoggesIn = true
+const debitCard = true
+
+if (userLoggesIn && debitCard && 2==2) {
+    console.log("Allowed to buy course");
+}
---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("It's January");
+        break;
+    case 2:
+        console.log("It's February");
+        break;
+    case 3:
+        console.log("It's March");
+        break;
+
+    default:
+        console.log("It's the deafult case");
+        break;
+}
