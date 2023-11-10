ab4bd884da43a08cbf0caa55bba0b2f687bc9a2d
kanishkmunot <128204622+kanishkmunot@users.noreply.github.com>
2023-11-10 08:22:04 +0000

    drake

--- .devcontainer/05_iterations/one.js
+++ .devcontainer/05_iterations/one.js
@@ -1,6 +1,16 @@
 // for
 
-for (let index = 0; index <= 10; index++) {
-    const element = index;
-    console.log(element);
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+// break and continue
+
+for (let index = 0; index <= 20; index++) {
+    if (index == 5) {
+        console.log("Detected 5");
+        break
+    }
+    console.log(`Value of i is ${index}`);
 }
