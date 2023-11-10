219ae3d027419d4135d5babfb50dd6796fc0d18c
kanishkmunot <128204622+kanishkmunot@users.noreply.github.com>
2023-11-10 08:26:00 +0000

    do while

--- .devcontainer/05_iterations/two.js
+++ .devcontainer/05_iterations/two.js
@@ -1,5 +1,12 @@
-let index = 0
-while (index <= 10) {
-    console.log(`Value of index is ${index}`);
-    index = index +2
-}
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index +2
+// }
+
+
+// let score = 10
+// do {
+//     console.log(`Score is ${score}`);
+//     score ++
+// } while (score<=10);
